955309299ca2da0e6f861bf13c76de8a67cb368a
PavelFlegr <pavelflegr@gmail.com>
2020-10-17 23:26:25 +0200

    Update target.js

--- target.js
+++ target.js
@@ -1,37 +1,5 @@
-let origin = "https://fiddle.jshell.net"
-let targetwindow
-let w = window
-let i
-function register() {
-	window.addEventListener("message", handshake)
-	i = setInterval(discover, 0)
-}
-
-function discover() {
-	w.postMessage("ping", origin)
-	if(w === w.parent) clearInterval(i)
-	w = w.parent
-}
-
-function handshake(e) {
-	if(!(e.origin === origin && e.data === "pong")) return
-	window.removeEventListener("message", handshake)
-	window.addEventListener("message", handler)
-	targetwindow = e.source
-	sendUpdate()
-	
-}
-
-function handler(e) {
-	sendUpdate()
-}
-
-function sendUpdate() {
-	targetwindow.postMessage(document.documentElement.scrollHeight, origin)
-}
-
-window.addEventListener("resize", (e) => {
-	sendUpdate()
+addEventListener("resize", () => {
+	postMessage(document.documentElement.scrollHeight, "*")
 })
 
-register(origin)
+postMessage(document.documentElement.scrollHeight, "*")
