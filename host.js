7f7bfe166aa44635573ae3434786ee6ece8a7353
PavelFlegr <pavelflegr@gmail.com>
2020-10-17 23:29:27 +0200

    Update host.js

--- host.js
+++ host.js
@@ -1,8 +1,8 @@
+function handler(e) {
+  if(e.origin !== "https://cuhumanities.az1.qualtrics.com") return
+  frame.height = e.data
+}
+
 function register(frame) {
-  function handler(e) {
-    if(e.origin !== "https://cuhumanities.az1.qualtrics.com") return
-    frame.height = e.data
-  }
-  
   window.addEventHandler("message", handler)
 }
