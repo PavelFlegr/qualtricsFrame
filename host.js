10b86fddd5f286d091f6ec5476b823983c8342fb
PavelFlegr <pavelflegr@gmail.com>
2020-10-18 01:54:10 +0200

    Update host.js

--- host.js
+++ host.js
@@ -1,4 +1,4 @@
-function register(frame) {
+export default function register(frame) {
   function handler(e) {
     if(e.origin !== "https://cuhumanities.az1.qualtrics.com") return
     this.height = e.data
